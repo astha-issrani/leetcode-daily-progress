18eb727440c78f43054d6cf57fc2f3071071c426
Astha Issrani <asthaissrani711@gmail.com>
2025-08-11 01:39:07 +0530

    added solution for reordered power of two

--- solutions/10aug/reorderedpoweroftwo.cpp
+++ solutions/10aug/reorderedpoweroftwo.cpp
@@ -1,4 +1,22 @@
 #include <stack>
 #include <string>
+#include <algorithm>
 using namespace std;
 
+class Solution {
+public:
+    string getsortednumber(int n){
+        string s=to_string(n);
+        sort(begin(s),end(s));
+        return s;
+    }
+    bool reorderedPowerOf2(int n) {
+        string s=getsortednumber(n);
+        for(int p=0;p<29;p++){
+            if(s==getsortednumber(1<<p)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
